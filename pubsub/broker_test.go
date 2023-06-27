0e942804965113b99c20e06c2e44babbbfa74fe0
Gabriel Brandão <29805002+brandaogabriel7@users.noreply.github.com>
2023-06-27 14:36:13 -0300

    test: add test cases with int message type

--- pubsub/broker_test.go
+++ pubsub/broker_test.go
@@ -42,3 +42,39 @@ func TestStringBroker(t *testing.T) {
 		})
 	}
 }
+
+func TestIntBroker(t *testing.T) {
+	var testCases = []struct {
+		queue string
+		data  int
+	}{
+		{"topic", 1},
+		{"topic2", 2},
+		{"another topic", 3},
+	}
+
+	for _, testCase := range testCases {
+		testname := fmt.Sprintf("publish to %s queue", testCase.queue)
+		t.Run(testname, func(t *testing.T) {
+			broker := pubsub.NewBroker[int]()
+
+			subscribers := []chan pubsub.Message[int]{
+				make(chan pubsub.Message[int]),
+				make(chan pubsub.Message[int]),
+				make(chan pubsub.Message[int]),
+			}
+
+			for _, subscriber := range subscribers {
+				broker.Subscribe(testCase.queue, subscriber)
+			}
+
+			broker.Publish(testCase.queue, testCase.data)
+
+			for _, subscriber := range subscribers {
+				if message := <-subscriber; message.Queue != testCase.queue || message.Data != testCase.data {
+					t.Errorf("expected message to be {Queue: \"%s\", Data: %d}, got %v", testCase.queue, testCase.data, message)
+				}
+			}
+		})
+	}
+}
