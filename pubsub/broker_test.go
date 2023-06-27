9ef4ca8861b03534245e603f928fa57c2cfdf7a4
Gabriel Brandão <29805002+brandaogabriel7@users.noreply.github.com>
2023-06-27 14:55:09 -0300

    test: add test cases for structs and multiple messages

--- pubsub/broker_test.go
+++ pubsub/broker_test.go
@@ -78,3 +78,104 @@ func TestIntBroker(t *testing.T) {
 		})
 	}
 }
+
+func TestStructBroker(t *testing.T) {
+	var testCases = []struct {
+		queue string
+		data  struct {
+			Name string
+			Age  int
+		}
+	}{
+		{"topic", struct {
+			Name string
+			Age  int
+		}{"John Doe", 30}},
+		{"topic2", struct {
+			Name string
+			Age  int
+		}{"Jane Doe", 25}},
+		{"another topic", struct {
+			Name string
+			Age  int
+		}{"John Smith", 40}},
+	}
+
+	for _, testCase := range testCases {
+		testname := fmt.Sprintf("publish to %s queue", testCase.queue)
+		t.Run(testname, func(t *testing.T) {
+			broker := pubsub.NewBroker[struct {
+				Name string
+				Age  int
+			}]()
+
+			subscribers := []chan pubsub.Message[struct {
+				Name string
+				Age  int
+			}]{
+				make(chan pubsub.Message[struct {
+					Name string
+					Age  int
+				}]),
+				make(chan pubsub.Message[struct {
+					Name string
+					Age  int
+				}]),
+				make(chan pubsub.Message[struct {
+					Name string
+					Age  int
+				}]),
+			}
+
+			for _, subscriber := range subscribers {
+				broker.Subscribe(testCase.queue, subscriber)
+			}
+
+			broker.Publish(testCase.queue, testCase.data)
+
+			for _, subscriber := range subscribers {
+				if message := <-subscriber; message.Queue != testCase.queue || message.Data != testCase.data {
+					t.Errorf("expected message to be {Queue: \"%s\", Data: %v}, got %v", testCase.queue, testCase.data, message)
+				}
+			}
+		})
+	}
+}
+
+func TestMultipleMessagesPublishing(t *testing.T) {
+	var testCases = []struct {
+		queue string
+		data  []string
+	}{
+		{"topic", []string{"hello world", "hello world 2", "hello world 3"}},
+		{"topic2", []string{"hello world 4", "hello world 5", "hello world 6"}},
+		{"another topic", []string{"hello world 7", "hello world 8", "hello world 9"}},
+	}
+
+	for _, testCase := range testCases {
+		testname := fmt.Sprintf("publish to %s queue", testCase.queue)
+		t.Run(testname, func(t *testing.T) {
+			broker := pubsub.NewBroker[string]()
+
+			subscribers := []chan pubsub.Message[string]{
+				make(chan pubsub.Message[string]),
+				make(chan pubsub.Message[string]),
+				make(chan pubsub.Message[string]),
+			}
+
+			for _, subscriber := range subscribers {
+				broker.Subscribe(testCase.queue, subscriber)
+			}
+
+			for _, data := range testCase.data {
+				broker.Publish(testCase.queue, data)
+
+				for _, subscriber := range subscribers {
+					if message := <-subscriber; message.Queue != testCase.queue || message.Data != data {
+						t.Errorf("expected message for to be {Queue: \"%s\", Data: \"%s\"}, got %v", testCase.queue, data, message)
+					}
+				}
+			}
+		})
+	}
+}
